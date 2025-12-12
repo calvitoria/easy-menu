1ce4d3dc7f5ab30bcd3078fe5901dc2472748716
Vitoria Meinerz <calvi.vitoria@gmail.com>
2025-12-12 17:24:29 -0300

    L-01: Create menuitems

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_12_194637) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_202042) do
+  create_table "menu_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "menu_id", null: false
+    t.string "name"
+    t.decimal "price", precision: 8, scale: 2, default: "0.0"
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
+
   create_table "menus", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "menu_items", "menus"
 end
