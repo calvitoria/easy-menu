fdeca9597f5f4f7574f6dd0f1cbd95f4c9bdc30a
Vitoria Meinerz <calvi.vitoria@gmail.com>
2025-12-12 20:16:17 -0300

    L-01: Refactor menus and menu_items controllers
    - Optimized database queries for menus and menu items
    - Use similar before_action for both controllers but keep it separated to improve readability
    - Applied shallow routing to resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "menus#index"
   resources :menus do
-    resources :menu_items
+    resources :menu_items, shallow: true
   end
-
-  resources :menu_items
 end
